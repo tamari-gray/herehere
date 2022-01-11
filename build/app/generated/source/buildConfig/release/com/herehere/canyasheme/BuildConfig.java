066a2112bab8112cee10877bbc2377ca78536bf5
tamari-gray <tamarigray97@gmail.com>
2022-01-12 12:08:29 +1300

    build for android

---
+++ build/app/generated/source/buildConfig/release/com/herehere/canyasheme/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.herehere.canyasheme;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.herehere.canyasheme";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0.1";
+}
