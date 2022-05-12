bcd4d637d96640ac0de389f694a5bfd87c153615
tamari-gray <tamarigray97@gmail.com>
2022-05-13 00:32:45 +1200

    fix show empty lobby if game is reset while playing

--- lib/screens/lobby.dart
+++ lib/screens/lobby.dart
@@ -11,20 +11,23 @@ class Lobby extends StatelessWidget {
   final LocationController _locationController = Get.find();
   final Database _database = Get.find();
 
+  _leaveGame(String _userId) async {
+    await _locationController.stopUpdatingLocationInDb(_userId);
+    await _userController.leaveGame();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Obx(() {
       final bool isAdmin = _userController.user.value.isAdmin;
       final _userId = _userController.userId.value;
+
       return Scaffold(
         appBar: AppBar(
           title: isAdmin ? Text('Select tagger') : Text('lobby'),
           actions: [
             ElevatedButton(
-              onPressed: () async {
-                await _locationController.stopUpdatingLocationInDb(_userId);
-                await _userController.leaveGame();
-              },
+              onPressed: () => _leaveGame(_userId),
               child: Text('leave game'),
             ),
             isAdmin
@@ -42,49 +45,63 @@ class Lobby extends StatelessWidget {
         floatingActionButton: isAdmin
             ? FloatingActionButton.extended(
                 onPressed: () async {
-                  await _database.playGame();
+                  if (_gameController.players
+                      .any((player) => player.isTagger)) {
+                    await _database.playGame();
+                  }
                 },
                 label: _gameController.players.any((player) => player.isTagger)
                     ? Text('Start game')
                     : Text('Select Tagger to start game'),
               )
             : Container(),
-        body: Container(
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: _gameController.players.length,
-              itemBuilder: (_, index) {
-                final player = _gameController.players[index];
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Text(
-                            '${player.username}: ${player.locationAccuracy}',
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                            ),
+        // checks if game gets reset while playing
+        body: _gameController.players.length == 0
+            ? Container(
+                child: Center(
+                  child: Text(
+                    'Please leave game and rejoin',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              )
+            : Container(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: _gameController.players.length,
+                    itemBuilder: (_, index) {
+                      final player = _gameController.players[index];
+                      return Card(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  '${player.username}: ${player.locationAccuracy}',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              isAdmin
+                                  ? Checkbox(
+                                      value: player.isTagger,
+                                      onChanged: (newValue) {
+                                        _database.chooseTagger(
+                                            player.id, player.isTagger);
+                                      },
+                                    )
+                                  : Container(),
+                            ],
                           ),
                         ),
-                        isAdmin
-                            ? Checkbox(
-                                value: player.isTagger,
-                                onChanged: (newValue) {
-                                  _database.chooseTagger(
-                                      player.id, player.isTagger);
-                                },
-                              )
-                            : Container(),
-                      ],
-                    ),
-                  ),
-                );
-              }),
-        ),
+                      );
+                    }),
+              ),
       );
     });
   }
