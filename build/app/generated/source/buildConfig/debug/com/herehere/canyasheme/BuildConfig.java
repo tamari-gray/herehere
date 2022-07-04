c05203155f361bad6e6aa39d296143bbc8fa5930
tamari-gray <tamarigray97@gmail.com>
2022-07-04 21:49:04 +1200

    get geolocator to work on android

--- build/app/generated/source/buildConfig/debug/com/herehere/canyasheme/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/herehere/canyasheme/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.herehere.canyasheme";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "3.2.1";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "3.2.4";
 }
